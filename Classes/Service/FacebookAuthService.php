c55a5dcdce73928fa8090ab9355e6fdde648f52e
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2014-04-15 23:28:19 +0000

    Initial commit (refs #244)

---
+++ Classes/Service/FacebookAuthService.php
@@ -0,0 +1,227 @@
+<?php
+namespace Visol\Vifbauth\Service;
+
+use TYPO3\CMS\Core\Exception;
+use TYPO3\CMS\Core\Utility\DebugUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+require_once(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('vifbauth') . 'Resources/PHP/facebook.php');
+
+class FacebookAuthService extends \TYPO3\CMS\Sv\AbstractAuthenticationService {
+
+	/**
+	 * @var \TYPO3\CMS\Core\Authentication\AbstractUserAuthentication
+	 */
+	public $pObj;
+
+	/**
+	 * @var array
+	 */
+	public $login = array();
+
+	/**
+	 * @var array
+	 */
+	public $authInfo = array();
+
+	/**
+	 * @var array
+	 */
+	public $db_user = array();
+
+	/**
+	 * @var array
+	 */
+	public $db_groups = array();
+
+	/**
+	 * @var boolean
+	 */
+	public $writeAttemptLog = TRUE;
+
+	/**
+	 * @var boolean
+	 */
+	public $writeDevLog = TRUE;
+
+	/**
+	 * @var array
+	 */
+	protected $response;
+
+	// Subtype of the service which is used to call the service.
+	public $mode = 'authUserFe';
+
+	/**
+	 * @var array
+	 */
+	protected $extensionConfiguration;
+
+	/**
+	 * @var \TYPO3\CMS\Core\Database\DatabaseConnection
+	 */
+	protected $databaseHandle;
+
+	/**
+	 * CONSTRUCTOR
+	 */
+	public function __construct() {
+		$sysPageObject = GeneralUtility::makeInstance('\TYPO3\CMS\Frontend\Page\PageRepository');
+		$rootLine = $sysPageObject->getRootLine(1);
+		$typoscriptParser = GeneralUtility::makeInstance('\TYPO3\CMS\Core\TypoScript\ExtendedTemplateService');
+		$typoscriptParser->tt_track = 0;
+		$typoscriptParser->init();
+		$typoscriptParser->runThroughTemplates($rootLine);
+		$typoscriptParser->generateConfig();
+		$typoScriptService = GeneralUtility::makeInstance('\TYPO3\CMS\Extbase\Service\TypoScriptService');
+		$this->extensionConfiguration = $typoScriptService->convertTypoScriptArrayToPlainArray($typoscriptParser->setup['plugin.']['tx_vifbauth.']);
+		$this->databaseHandle = $GLOBALS['TYPO3_DB'];
+	}
+
+	/**
+	 * Inits some variables
+	 *
+	 * @return	void
+	 */
+	public function init() {
+		return parent::init();
+	}
+
+	/**
+	 * Initializes authentication for this service.
+	 *
+	 * @param string $mode: Subtype for authentication (either "getUserFE" or "getUserBE")
+	 * @param array $loginData: Login data submitted by user and preprocessed by AbstractUserAuthentication
+	 * @param array $authInfo: Additional TYPO3 information for authentication services (unused here)
+	 * @param \TYPO3\CMS\Core\Authentication\AbstractUserAuthentication $pObj Calling object
+	 * @return void
+	 */
+	public function initAuth($mode, array $loginData, array $authInfo, \TYPO3\CMS\Core\Authentication\AbstractUserAuthentication $pObj) {
+		if (defined('TYPO3_cliMode')) {
+			return parent::initAuth($mode, $loginData, $authInfo, $pObj);
+		}
+		// Store login and authentication data
+		$this->loginData = $loginData;
+		$this->authInfo = $authInfo;
+		$this->pObj = &$pObj;
+	}
+
+	/**
+	 * @param array $user
+	 * @return array
+	 */
+	public function authUser(array $user) {
+		$result = 100;
+
+		if ($this->isFacebookLogin() && !empty($user)) {
+			$result = 200;
+		}
+
+		return $result;
+	}
+
+	public function isFacebookLogin() {
+		$facebookConfiguration = array(
+			'appId'  => $this->extensionConfiguration['settings']['facebookAppId'],
+			'secret' => $this->extensionConfiguration['settings']['facebookAppSecret'],
+		);
+		$facebook = GeneralUtility::makeInstance('\Facebook', $facebookConfiguration);
+		return $facebook->getUser() > 0 ? TRUE : FALSE;
+	}
+
+	/**
+	 * @return array User Array or FALSE
+	 */
+	public function getUser() {
+		$user = FALSE;
+
+		$facebookConfiguration = array(
+			'appId'  => $this->extensionConfiguration['settings']['facebookAppId'],
+			'secret' => $this->extensionConfiguration['settings']['facebookAppSecret'],
+		);
+		$facebook = GeneralUtility::makeInstance('\Facebook', $facebookConfiguration);
+		$facebookUserId = $facebook->getUser();
+
+		if ($facebookUserId > 0) {
+			// TODO make use of it
+			$additionalFields = array(
+				'fields' => 'picture',
+				'type' => 'large'
+			);
+			$facebookUserInformation = $facebook->api('/me');
+
+			// we have an authenticated Facebook user
+			$user = $this->getFrontendUser($facebookUserId);
+			if (!is_array($user) || empty($user)) {
+				$this->createFrontendUser($facebookUserInformation);
+			} else {
+				$this->updateFrontendUser($user['uid'], $facebookUserInformation);
+			}
+			$user = $this->getFrontendUser($facebookUserId);
+		}
+
+		return $user;
+	}
+
+	/**
+	 * Get a frontend user
+	 *
+	 * @param $userId
+	 * @return array
+	 */
+	protected function getFrontendUser($userId) {
+		//$this->databaseHandle->store_lastBuiltQuery = TRUE;
+		$row = $this->databaseHandle->exec_SELECTgetSingleRow('*', 'fe_users', 'disable=0 AND deleted=0 AND pid=' . $this->extensionConfiguration['persistence']['storagePid'] . ' AND username=\'' . $userId . '\'');
+		return $row;
+
+	}
+
+	/**
+	 * @param $userInformation
+	 */
+	protected function createFrontendUser($userInformation) {
+		//$this->writelog(255,3,3,2, "Importing user %s!", array($eventoId));
+		$user = array(
+			'crdate' => time(),
+			'pid' => $this->extensionConfiguration['persistence']['storagePid'],
+
+			'tstamp' => time(),
+			'username' => $userInformation['id'],
+			'password' => md5(GeneralUtility::shortMD5(uniqid(rand(), TRUE))),
+			'email' => $userInformation['email'],
+			'first_name' => $userInformation['first_name'],
+			'last_name' => $userInformation['last_name'],
+			'usergroup' => (string)$this->extensionConfiguration['settings']['defaultFrontendUserGroupUid'],
+			'city' => $userInformation['location']['name'],
+			'tx_extbase_type' => 'Tx_Easyvote_CommunityUser',
+		);
+		$this->databaseHandle->exec_INSERTquery('fe_users', $user);
+	}
+
+	/**
+	 * Update an existing FE user
+	 *
+	 * @param $userId
+	 * @param $userInformation
+	 * @return void
+	 */
+	protected function updateFrontendUser($userId, $userInformation) {
+		//$this->writelog(255,3,3,2,	"Updating user %s!", array($eventoId));
+		$where = "uid = " . $userId;
+		$user = array(
+			'tstamp' => time(),
+			'username' => $userInformation['id'],
+			'password' => md5(GeneralUtility::shortMD5(uniqid(rand(), TRUE))),
+			'email' => $userInformation['email'],
+			'first_name' => $userInformation['first_name'],
+			'last_name' => $userInformation['last_name'],
+			'usergroup' => (string)$this->extensionConfiguration['settings']['defaultFrontendUserGroupUid'],
+			'city' => $userInformation['location']['name'],
+			'tx_extbase_type' => 'Tx_Easyvote_CommunityUser',
+		);
+		$this->databaseHandle->exec_UPDATEquery('fe_users', $where, $user);
+	}
+
+}
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,35 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+	'Visol.' . $_EXTKEY,
+	'Auth',
+	array(
+		'Authentication' => 'login',
+	),
+	// non-cacheable actions
+	array(
+		'Authentication' => 'login',
+	)
+);
+
+// Register opauth as authentication service
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService($_EXTKEY, 'auth', 'Visol\\Opauth\\Service\\OpauthService',
+	array(
+		'title' => 'Facebook Authentication',
+		'description' => 'Facebook Authentication for Frontend',
+		'subtype' => 'getUserFE,authUserFE',
+		'available' => TRUE,
+		// Must be higher than for tx_sv_auth (50) or tx_sv_auth will deny request unconditionally
+		'priority' => 90,
+		'quality' => 90,
+		'os' => '',
+		'exec' => '',
+		'className' => 'Visol\\Vifbauth\\Service\\FacebookAuthService',
+		'classFile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Service/FacebookAuthService.php'
+	)
+);
+
+?>
